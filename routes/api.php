e64cdd756ae1f190af9763f2053dfe4157987925
RenzeD98 <renze.d98@gmail.com>
2019-05-16 15:16:56 +0200

    added projects controller and cleaned up api.php routes

--- routes/api.php
+++ routes/api.php
@@ -15,30 +15,29 @@ use Illuminate\Http\Request;
 
 //login
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('login', 'API\AuthController@login');
-    Route::post('signup', 'API\AuthController@signup');
+    Route::post(    'login',                            'API\AuthController@login');
+    Route::post(    'signup',                           'API\AuthController@signup');
 
     Route::group(['middleware' => 'auth:api'], function() {
-        Route::get('logout', 'API\AuthController@logout');
-        Route::get('user', 'API\AuthController@user');
+        Route::get( 'logout',                           'API\AuthController@logout');
+        Route::get( 'user',                             'API\AuthController@user');
     });
 });
 
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::post('get-all-videos', 'API\VideoController@getAllVideos');
-    Route::post('add-video', 'API\VideoController@addVideo');
-    Route::patch('update-video/{id}', 'API\VideoController@updateVideo');
-    Route::delete('delete-video/{id}', 'API\VideoController@deleteVideo');
+    Route::get(     'get-all-videos/{project-id?}',     'API\VideoController@getAllVideos');
+    Route::post(    'add-video',                        'API\VideoController@addVideo');
+    Route::patch(   'update-video/{id}',                'API\VideoController@updateVideo');
+    Route::delete(  'delete-video/{id}',                'API\VideoController@deleteVideo');
 
-    Route::post('/get-all-video-notes', 'API\VideoNoteController@getAllVideoNotes');
-    Route::post('/create-video-note', 'API\VideoNoteController@createVideoNote');
-    Route::patch('/update-video-note/{id}', 'API\VideoNoteController@updateVideoNote');
-    Route::delete('/delete-video-note/{id}', 'API\VideoNoteController@deleteVideoNote');
-});
-
+    Route::get(     'get-all-projects/{company-id?}',   'API\ProjectController@getAllProjects');
+    Route::post(    'add-project',                      'API\ProjectController@addProject');
+    Route::patch(   'update-project/{id}',              'API\ProjectController@updateProject');
+    Route::delete(  'delete-project/{id}',              'API\ProjectController@deleteProject');
 
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get(     '/get-all-video-notes/{video-id?}', 'API\VideoNoteController@getAllVideoNotes');
+    Route::post(    '/create-video-note',               'API\VideoNoteController@createVideoNote');
+    Route::patch(   '/update-video-note/{id}',          'API\VideoNoteController@updateVideoNote');
+    Route::delete(  '/delete-video-note/{id}',          'API\VideoNoteController@deleteVideoNote');
 });
