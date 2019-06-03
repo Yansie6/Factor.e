3448a83fa00ff2e9342b4f81929c369c2d50d7f9
RenzeD98 <renze.d98@gmail.com>
2019-06-03 09:38:39 +0200

    changed patch method

--- routes/api.php
+++ routes/api.php
@@ -27,32 +27,32 @@ Route::group(['prefix' => 'auth'], function () {
 
     Route::get(     'get-all-videos/{projectId?}',          'API\VideoController@getAllVideos');
     Route::post(    'create-video',                         'API\VideoController@createVideo');
-    Route::patch(   'update-video/{videoId}',               'API\VideoController@updateVideo');
+    Route::post(   'update-video/{videoId}',               'API\VideoController@updateVideo');
     Route::delete(  'delete-video/{videoId}',               'API\VideoController@deleteVideo');
 
     Route::get(     'get-all-projects/{companyId?}',        'API\ProjectController@getAllProjects');
     Route::post(    'create-project',                       'API\ProjectController@createProject');
-    Route::patch(   'update-project/{projectId}',           'API\ProjectController@updateProject');
+    Route::post(   'update-project/{projectId}',           'API\ProjectController@updateProject');
     Route::delete(  'delete-project/{projectId}',           'API\ProjectController@deleteProject');
 
     Route::get(     'get-all-notes/{projectId?}',           'API\NoteController@getAllNotes');
     Route::post(    'create-note',                          'API\NoteController@createNote');
-    Route::patch(   'update-note/{noteId}',                 'API\NoteController@updateNote');
+    Route::post(   'update-note/{noteId}',                 'API\NoteController@updateNote');
     Route::delete(  'delete-note/{noteId}',                 'API\NoteController@deleteNote');
 
     Route::get(     'get-all-video-notes/{videoId?}',       'API\VideoNoteController@getAllVideoNotes');
     Route::post(    'create-video-note',                    'API\VideoNoteController@createVideoNote');
-    Route::patch(   'update-video-note/{videoNoteId}',      'API\VideoNoteController@updateVideoNote');
+    Route::post(   'update-video-note/{videoNoteId}',      'API\VideoNoteController@updateVideoNote');
     Route::delete(  'delete-video-note/{videoNoteId}',      'API\VideoNoteController@deleteVideoNote');
 
     Route::get(     'get-all-companies',                    'API\companyController@getAllCompanies');
     Route::post(    'create-company',                       'API\companyController@createCompany');
-    Route::patch(   'update-company/{companyId}',           'API\companyController@updateCompany');
+    Route::post(   'update-company/{companyId}',           'API\companyController@updateCompany');
     Route::delete(  'delete-company/{companyId}',           'API\companyController@deleteCompany');
 
         Route::get(     'get-all-notes-t/{projectId?}',           'API\NoteControllerRefactor@getAllNotes');
         Route::post(    'create-note-t',                          'API\NoteControllerRefactor@createNote');
-        Route::patch(   'update-note-t/{noteId}',                 'API\NoteControllerRefactor@updateNote');
+        Route::post(   'update-note-t/{noteId}',                 'API\NoteControllerRefactor@updateNote');
         Route::delete(  'delete-note-t/{noteId}',                 'API\NoteControllerRefactor@deleteNote');
 
 Route::group(['middleware' => 'auth:api'], function() {
@@ -61,28 +61,5 @@ Route::group(['middleware' => 'auth:api'], function() {
 
     Route::get(     'get/{type}/{linkedId?}',                   'API@get');
     Route::post(    'create/{type}',                            'API@create');
-    Route::patch(   'update/{type}/{id}',                       'API@update');
+    Route::post(   'update/{type}/{id}',                       'API@update');
     Route::delete(  'delete/{type}/{id}',                       'API@delete');
-
-
-//DIT KAN WEG :=)
-//
-//Route::get(     'get/{project}/{companyId?}',        'API@getAll');
-//Route::post(    'create-project',                       'API@create');
-//Route::patch(   'update-project/{projectId}',           'API@update');
-//Route::delete(  'delete-project/{projectId}',           'API@delete');
-//
-//Route::get(     'get-all-notes/{projectId?}',           'API@getAll');
-//Route::post(    'create-note',                          'API@create');
-//Route::patch(   'update-note/{noteId}',                 'API@update');
-//Route::delete(  'delete-note/{noteId}',                 'API@delete');
-//
-//Route::get(     'get-all-video-notes/{videoId?}',       'API@getAll');
-//Route::post(    'create-video-note',                    'API@create');
-//Route::patch(   'update-video-note/{videoNoteId}',      'API@update');
-//Route::delete(  'delete-video-note/{videoNoteId}',      'API@delete');
-//
-//Route::get(     'get-all-companies',                    'API@getAll');
-//Route::post(    'create-company',                       'API@create');
-//Route::patch(   'update-company/{companyId}',           'API@update');
-//Route::delete(  'delete-company/{companyId}',           'API@delete');
